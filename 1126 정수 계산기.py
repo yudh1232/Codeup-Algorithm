5dc249d85fe82974817adf9d073e4df0b47bb14a
yudh1232 <86055115+yudh1232@users.noreply.github.com>
2021-06-25 17:03:09 +0900

    Create 1126 정수 계산기.py

---
+++ 1126 정수 계산기.py
@@ -0,0 +1,7 @@
+a, b = map(int, input().split())
+
+print("{0} + {1} = {2}".format(a, b, a + b))
+print("{0} - {1} = {2}".format(a, b, a - b))
+print("{0} * {1} = {2}".format(a, b, a * b))
+print("{0} / {1} = {2}".format(a, b, a // b))
+print("{0} % {1} = {2}".format(a, b, a % b))
