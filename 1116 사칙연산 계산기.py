4235376c19e74ebbc4d8e9b5606f17d06de0b725
yudh1232 <86055115+yudh1232@users.noreply.github.com>
2021-06-25 16:21:46 +0900

    Create 1116 사칙연산 계산기.py

---
+++ 1116 사칙연산 계산기.py
@@ -0,0 +1,6 @@
+a, b = map(int, input().split())
+
+print("{0}+{1}={2}".format(a, b, a + b))
+print("{0}-{1}={2}".format(a, b, a - b))
+print("{0}*{1}={2}".format(a, b, a * b))
+print("{0}/{1}={2}".format(a, b, a // b))
