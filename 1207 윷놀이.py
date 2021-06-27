3f5d572463ca868fbd719d05e052fdad60addcea
yudh1232 <86055115+yudh1232@users.noreply.github.com>
2021-06-27 16:13:52 +0900

    Create 1207 윷놀이.py

---
+++ 1207 윷놀이.py
@@ -0,0 +1,14 @@
+a, b, c, d = map(int, input().split())
+
+sum = a + b + c + d
+
+if sum == 0:
+  print("모")
+elif sum == 1:
+  print("도")
+elif sum == 2:
+  print("개")
+elif sum == 3:
+  print("걸")
+elif sum == 4:
+  print("윷")
